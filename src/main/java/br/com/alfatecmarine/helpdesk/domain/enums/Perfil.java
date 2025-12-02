87536077a1425e2734a086c190727bd510bb783a
rapferraz <rapferrazsbsp@gmail.com>
2025-12-02 09:33:40 -0300

    enum perfil adicionado

---
+++ src/main/java/br/com/alfatecmarine/helpdesk/domain/enums/Perfil.java
@@ -0,0 +1,5 @@
+package br.com.alfatecmarine.helpdesk.domain.enums;
+
+public enum Perfil {
+ // para quando eu retornar: paramos na parte de criar os enums, acabamos de criar o de Perfil, depois de olhar o modelo conceitual do projeto.
+}
